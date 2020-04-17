2bb7ee5c955703d22e33308159224c30ca12ecb1
Adam Misiak <adammisiak3@gmail.com>
2020-04-17 17:08:47 +0200

    code refactored

--- file_cleaner.py
+++ file_cleaner.py
@@ -4,16 +4,16 @@ from collections import Counter
 
 def file_sorter():
 
-    # ADD HERE PATH TO CATALOG YOU WANT TO SORT
-    dir_to_sort = "C:\\Users\\adamm\\Desktop\\TEST\\all"
-    main_dir = "C:\\Users\\adamm\\Desktop\\TEST"
+    # CHANGE THE PATH YOU WANT TO SORT AND WHERE YOU WANT TO SAVE FOLDERS
+    dir_to_sort = "C:\\Users\\USERNAME\\Desktop\\TEST\\all"
+    dir_to_save = "C:\\Users\\USERNAME\\Desktop\\TEST"
     files_list = os.listdir(dir_to_sort)
     extension_list = []
 
     for file in files_list:
         extension = file.split('.')[-1]
         source = dir_to_sort + '\\\\' + str(file)
-        extension_dir = main_dir + '\\\\' + extension
+        extension_dir = dir_to_save + '\\\\' + extension
 
         # CREATING NEW FOLDERS FOR EXTENSIONS
         try:
@@ -37,6 +37,6 @@ def file_sorter():
     if counted_extensions_dict == {}:
         print('No files to sort')
     else:
-        print(f'All folders created in {main_dir}')
+        print(f'All folders created in {dir_to_save}')
 
 file_sorter()
