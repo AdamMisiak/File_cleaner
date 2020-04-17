913754275fcfa9d0aee5f1a98b2aabf9c6745a65
Adam Misiak <adammisiak3@gmail.com>
2020-04-17 15:58:25 +0200

    identifying extensions of files

--- file_sorter.py
+++ file_sorter.py
@@ -5,35 +5,39 @@ def file_sorter():
     txt_counter, pdf_counter, exe_counter, not_moved = 0, 0, 0, 0
 
     # ADD HERE PATH TO CATALOG YOU WANT TO SORT
-    files_to_sort = "C:\\Users\\YOURNAME\\Desktop\\TEST\\all"
-    files_list = os.listdir(files_to_sort)
+    dir_to_sort = "C:\\Users\\adamm\\Desktop\\TEST\\all"
+    main_dir = "C:\\Users\\adamm\\Desktop\\TEST"
+    files_list = os.listdir(dir_to_sort)
+
 
     for file in files_list:
-        extension = file[-3:]
-        source = files_to_sort+str(file)
+        extension = file.split('.')[-1]
+        source = dir_to_sort + '\\\\' + str(file)
+        extension_dir = main_dir + '\\\\' + extension
+        try:
+            os.mkdir(extension_dir)
+        except FileExistsError:
+            pass
 
-        # ADD HERE PATH TO CATALOGS YOU WANT TO MOVE YOUR SORTED FILES
-        txt_destination = "C:\\Users\\YOURNAME\\Desktop\\TEST\\txt"
-        pdf_destination = "C:\\Users\\YOURNAME\\Desktop\\TEST\\pdf"
-        exe_destination = "C:\\Users\\YOURNAME\\Desktop\\TEST\\exe"
-        # ADD MORE EXTENSIONS HERE
+        shutil.move(source, extension_dir)
+zrob liste dodajaca ext i potem zliczanie ilosci
 
 
-        if extension == "txt":
-            shutil.move(source, txt_destination)
-            txt_counter += 1
-        elif extension == "pdf":
-            shutil.move(source, pdf_destination)
-            pdf_counter += 1
-        elif extension == "exe":
-            shutil.move(source, exe_destination)
-            exe_counter += 1
-        # ADD HERE MORE ELIFS FOR NEW EXTENSIONS OF FILES
-        else:
-            not_moved += 1
+        # if extension == "txt":
+        #     shutil.move(source, txt_destination)
+        #     txt_counter += 1
+        # elif extension == "pdf":
+        #     shutil.move(source, pdf_destination)
+        #     pdf_counter += 1
+        # elif extension == "exe":
+        #     shutil.move(source, exe_destination)
+        #     exe_counter += 1
+        # # ADD HERE MORE ELIFS FOR NEW EXTENSIONS OF FILES
+        # else:
+        #     not_moved += 1
 
-    print(f"Sorting done: \n {txt_counter} txt files sorted \n {pdf_counter} pdf files sorted \n" \
-          f" {exe_counter} exe files sorted \n {not_moved} files not sorted")
+    # print(f"Sorting done: \n {txt_counter} txt files sorted \n {pdf_counter} pdf files sorted \n" \
+    #       f" {exe_counter} exe files sorted \n {not_moved} files not sorted")
 
 
 
